901860ad651823cd77897517e58a3308a984c995
eunjeong <eunjeong990731@gmail.com>
2023-12-06 17:56:53 +0900

    :memo: Modify : ts 컴파일 전 커밋

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
     const datepicker = document.getElementById('datepicker') as HTMLInputElement;
     const astronomyInfoContainer = document.getElementById('astronomyInfo');
-  
+
     function getAstronomyPicture() {
-      const apiKey = 'kPaqXTtaN1YmenxQ6wEdzTovcXk8iv7fa7EMS9c8';
-      const dateInput = datepicker.value;
-  
-      if (!dateInput) {
-        alert('Please select a date.');
-        return;
-      }
-  
-      const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${dateInput}`;
-  
-      fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => displayAstronomyInfo(data))
-        .catch(error => console.error('Error fetching data:', error));
+        const apiKey = 'kPaqXTtaN1YmenxQ6wEdzTovcXk8iv7fa7EMS9c8';
+        const dateInput = datepicker.value;
+
+        if (!dateInput) {
+            alert('Please select a date.');
+            return;
+        }
+
+        const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${dateInput}`;
+
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => displayAstronomyInfo(data))
+            .catch(error => console.error('Error fetching data:', error));
     }
-  
+
     function displayAstronomyInfo(data: { title: string; url: string }) {
-      astronomyInfoContainer.innerHTML = `
-          <h2>${data.title}</h2>
-          <img src="${data.url}" alt="${data.title}" style="max-width: 70%;">
-      `;
+        // Optional Chaining을 사용하여 null 체크
+        astronomyInfoContainer?.innerHTML = `
+            <h2>${data.title}</h2>
+            <img src="${data.url}" alt="${data.title}" style="max-width: 70%;">
+        `;
     }
-  
-    document.getElementById('astronomyInfo')?.addEventListener('click', getAstronomyPicture);
-  });
-  
+
+    // astronomyInfoContainer가 null이 아닌 경우에만 이벤트 리스너 등록
+    astronomyInfoContainer?.addEventListener('click', getAstronomyPicture);
+});
