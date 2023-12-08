60ec45280d4220822ea0305b885f564be316defa
eunjeong <eunjeong990731@gmail.com>
2023-12-08 10:55:25 +0900

    🗑️ RemoveF : 파일 삭제
    컴포넌트를 나누는것은 react 버전 지금의 나는 컴포넌트를 한 곳에 작성하기에 해당 파일들이 필요하지 않다.

--- src/components/AstronomyInfo.js
+++
@@ -1,9 +0,0 @@
-// AstronomyInfo.js
-export default function AstronomyInfo(title, url) {
-  return (
-    <div id="astronomyInfo">
-      <h2>{title}</h2>
-      <img src={url} alt={title} style={{ maxWidth: '70%' }} />
-    </div>
-  );
-}
